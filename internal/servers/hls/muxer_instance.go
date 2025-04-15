487d49a34a3b916eb5a2e2d748a30345c0229efd
SteveHoffmannCarl <sh@carl-group.de>
2025-04-15 19:14:53 +0200

    Update muxer_instance.go

--- internal/servers/hls/muxer_instance.go
+++ internal/servers/hls/muxer_instance.go
@@ -1,8 +1,11 @@
 package hls
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/bluenviron/gohlslib/v2"
@@ -59,28 +62,53 @@ func (mi *muxerInstance) initialize() error {
 		return err
 	}
 
-	// Inject subtitle playlist reference after small delay to ensure index.m3u8 exists
-	go func() {
-		time.Sleep(2 * time.Second)
-
-		indexPath := filepath.Join(muxerDirectory, "index.m3u8")
-		f, err := os.OpenFile(indexPath, os.O_APPEND|os.O_WRONLY, 0644)
-		if err == nil {
-			defer f.Close()
-			subtitleLine := "#EXT-X-MEDIA:TYPE=SUBTITLES,GROUP-ID=\"subs\",NAME=\"Deutsch\",LANGUAGE=\"de\",AUTOSELECT=YES,DEFAULT=YES,URI=\"subtitles.vtt\"\n"
-			f.WriteString(subtitleLine)
-		}
-	}()
-
 	mi.Log(logger.Info, "is converting into HLS, %s",
 		defs.FormatsInfo(mi.stream.ReaderFormats(mi)))
 
 	mi.stream.StartReader(mi)
 
+	// Inject subtitle reference after short delay (to ensure muxer has started)
+	go func() {
+		time.Sleep(2 * time.Second)
+		mi.injectSubtitleReference(muxerDirectory)
+	}()
+
 	return nil
 }
 
-// Log implements logger.Writer.
+// injectSubtitleReference appends EXT-X-MEDIA and updated EXT-X-STREAM-INF for subtitles
+func (mi *muxerInstance) injectSubtitleReference(dir string) {
+	mainPlaylist := filepath.Join(dir, "main_stream.m3u8")
+	info, err := os.Stat(mainPlaylist)
+	if err != nil || info.IsDir() {
+		return
+	}
+
+	b, err := ioutil.ReadFile(mainPlaylist)
+	if err != nil {
+		mi.Log(logger.Warn, "unable to read playlist: %v", err)
+		return
+	}
+
+	content := string(b)
+
+	// Check if EXT-X-MEDIA is already present
+	if strings.Contains(content, "EXT-X-MEDIA:TYPE=SUBTITLES") {
+		return
+	}
+
+	subsLine := `#EXT-X-MEDIA:TYPE=SUBTITLES,GROUP-ID="subs",NAME="Deutsch",LANGUAGE="de",AUTOSELECT=YES,DEFAULT=YES,URI="subtitles.m3u8"`
+	infLine := `#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=800000,SUBTITLES="subs"`
+	
+	// prepend subtitle metadata
+	newContent := "#EXTM3U\n#EXT-X-VERSION:3\n" + subsLine + "\n" + infLine + "\n" + filepath.Base(mainPlaylist) + "\n"
+	
+	err = ioutil.WriteFile(mainPlaylist, []byte(newContent), 0644)
+	if err != nil {
+		mi.Log(logger.Warn, "unable to write updated playlist: %v", err)
+	}
+}
+
 func (mi *muxerInstance) Log(level logger.Level, format string, args ...interface{}) {
 	mi.parent.Log(level, format, args...)
 }
